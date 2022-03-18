81df783fcdaa10efd10a20e1d03ce6b47a0973b7
Appiva-Tech <brscntyz@gmail.com>
2022-03-18 20:44:52 +0300

    first comment

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  
+
+  return (
+    <div className="App">
+      <h2>Let's get started!</h2>
+      <p>This is also visible</p>
+    </div>
+  );
+}
+
+export default App;
